885dd28fad2b5729f399b0c46bb0819fe83b10f0
Alex Moller <amloelxer@gmail.com>
2023-07-25 20:03:39 -0600

    update .env files

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 
+	"github.com/joho/godotenv"
 	"whatisalexlisteningto.com/api/utils"
 )
 
 func main() {
-	tempArray := []string{"one", "two", "three"}
-	fmt.Println(utils.ArrayToString(tempArray))
 	customLogger := utils.GetLogger()
 	customLogger.Infoln("Hello World from sugar logger!")
+	err := godotenv.Load()
+	if err != nil {
+		customLogger.Fatal("Error loading .env file")
+	}
+	tempArray := []string{"one", "two", "three"}
+	fmt.Println(utils.ArrayToString(tempArray))
 }
