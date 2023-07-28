769c88ebc443c6e52f9a3fa00dd9950744e521e9
Alex Moller <amloelxer@gmail.com>
2023-07-28 13:20:09 -0600

    try to figure out this heinous db issue later where I need to disable ssl scream

--- main.go
+++ main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"whatisalexlisteningto.com/api/models"
 	"whatisalexlisteningto.com/api/utils"
 )
 
+func connectDB() *sql.DB {
+	db, err := sql.Open("postgres", "postgresql://alex:password@127.0.0.1/what-am-i-listening-to?tLSMode=0")
+	if err != nil {
+		utils.GetLogger().Fatal("Die")
+	}
+	return db
+}
+
 func main() {
+	ctx := context.Background()
 	customLogger := utils.GetLogger()
 	customLogger.Infoln("Hello World from sugar logger!")
 	err := godotenv.Load()
@@ -23,6 +37,12 @@ func main() {
 	fakeSecretKey := os.Getenv("FAKE_SECRET_KEY")
 	customLogger.Infoln("The fake secret key is", fakeSecretKey)
 
+	db := connectDB()
+	boil.SetDB(db)
+
+	artists, err := models.Artists().All(ctx, db)
+	fmt.Println(artists)
+	fmt.Println(err.Error())
 	r := gin.Default()
 	// albumsArray := []models.Album{{Name: "Stadium Arcadium", Artist: "Red Hot Chili Peppers"}}
 	r.GET("/albums", func(c *gin.Context) {
