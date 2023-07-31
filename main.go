d0e4a376495d4b1d4931edba761ee7a4368528fd
Alex Moller <amloelxer@gmail.com>
2023-07-31 09:11:58 -0600

    figured out my ssl issue, return a valid payload with my api

--- main.go
+++ main.go
@@ -11,14 +11,25 @@ import (
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"go.uber.org/zap"
 	"whatisalexlisteningto.com/api/models"
 	"whatisalexlisteningto.com/api/utils"
 )
 
-func connectDB() *sql.DB {
-	db, err := sql.Open("postgres", "postgresql://alex:password@127.0.0.1/what-am-i-listening-to?tLSMode=0")
+type person struct {
+	name string
+	age  int
+}
+
+func (inputPerson *person) ToString() string {
+	myString := fmt.Sprintf("The name of my person is %s and the age is %v", inputPerson.name, inputPerson.age)
+	return myString
+}
+
+func connectDB(logger *zap.SugaredLogger) *sql.DB {
+	db, err := sql.Open("postgres", "postgresql://alex:password@127.0.0.1/what-am-i-listening-to?sslmode=disable")
 	if err != nil {
-		utils.GetLogger().Fatal("Die")
+		logger.Fatal("Die")
 	}
 	return db
 }
@@ -34,22 +45,32 @@ func main() {
 	tempArray := []string{"one", "two", "three"}
 	fmt.Println(utils.ArrayToString(tempArray))
 
+	myFakePerson := &person{name: "Alex", age: 29}
+	customLogger.Infoln(myFakePerson.ToString())
+
 	fakeSecretKey := os.Getenv("FAKE_SECRET_KEY")
 	customLogger.Infoln("The fake secret key is", fakeSecretKey)
 
-	db := connectDB()
+	db := connectDB(customLogger)
 	boil.SetDB(db)
 
 	artists, err := models.Artists().All(ctx, db)
-	fmt.Println(artists)
-	fmt.Println(err.Error())
+	if err != nil {
+		customLogger.Panicln("The heinous error is", err.Error())
+	}
+
+	for _, artist := range artists {
+		fmt.Println("The artist name is", artist.Name.String)
+		fmt.Println("The artist biography is", artist.Biography.String)
+	}
+
+	fmt.Println("The value of the artists is", artists)
+	// fmt.Println(err.Error())
 	r := gin.Default()
 	// albumsArray := []models.Album{{Name: "Stadium Arcadium", Artist: "Red Hot Chili Peppers"}}
-	r.GET("/albums", func(c *gin.Context) {
+	r.GET("/artists", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"data": gin.H{
-				"okay": true,
-			},
+			"data": artists,
 		})
 	})
 	const address = "localhost:3001"
